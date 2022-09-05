1a59ef3a9ad2adc7b9f20d0348d02a123f40c02d
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2022-09-05 22:55:06 +0300

    fixed MainWindow loading

--- src/dataset_image_annotator/__main__.py
+++ src/dataset_image_annotator/__main__.py
@@ -2,9 +2,9 @@ import argparse
 import sys
 from pathlib import Path
 
+from PySide6.QtCore import QFile, QIODevice
+from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
-
-from dataset_image_annotator.main_window import MainWindow
 
 
 def get_parsed_args():
@@ -21,8 +21,24 @@ def main():
     data_root_path = Path(args.data_root)
 
     app = QApplication(sys.argv)
-    widget = MainWindow()
-    widget.show()
+
+    ui_file_name = 'main_window.ui'
+    ui_file = QFile(Path(__file__).resolve().parent / ui_file_name)
+
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f'Cannot open {ui_file_name}: {ui_file.errorString()}')
+        sys.exit(-1)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
+
+    window.show()
+
     sys.exit(app.exec())
 
 
