8073a71e5741805a0e47e2952dae9343dd95de8a
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2023-01-06 14:50:29 +0200

    passing logging config to uvicorn, json

--- src/dataset_image_annotator/api/__init__.py
+++ src/dataset_image_annotator/api/__init__.py
@@ -1,8 +1,38 @@
+import logging.config
+
 import uvicorn
 
 from dataset_image_annotator.api.http import app
 from dataset_image_annotator.conf import settings
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host=settings.service_addr, port=settings.service_port)
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            '()': 'dataset_image_annotator.formatter.JSONFormatter',
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': settings.logging_level,
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'default',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default', ],
+        },
+        'dataset_image_annotator': {
+            'handlers': ['default', ],
+            'level': settings.logging_level,
+            'propagate': False,
+        }
+    }
+}
+logging.config.dictConfig(LOGGING_CONFIG)
+
+uvicorn.run(app, host=settings.service_addr, port=settings.service_port, proxy_headers=True, log_config=LOGGING_CONFIG)
