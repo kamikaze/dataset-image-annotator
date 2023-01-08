72073cfd65d1dc370cef6243674c7e0fb4ce3031
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2023-01-09 01:55:53 +0200

    added initial migration

--- src/dataset_image_annotator/db/models.py
+++ src/dataset_image_annotator/db/models.py
@@ -1,7 +1,7 @@
 import enum
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
-from sqlalchemy import Column, String, DateTime, ForeignKey, BIGINT, UniqueConstraint, Integer
+from sqlalchemy import Column, String, DateTime, ForeignKey, BIGINT, UniqueConstraint, Integer, SmallInteger
 from sqlalchemy.dialects.postgresql import JSONB, ENUM
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql import expression
@@ -45,9 +45,9 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 class ImageSample(BaseDBModel, Base):
     __tablename__ = 'image_samples'
 
+    filename = Column(String, nullable=False)
     checksum = Column(String, nullable=False)
     location = Column(String, nullable=False, unique=True)
-    metadata = Column(JSONB)
 
 
 class AnnotationKeyEnum(enum.Enum):
@@ -70,3 +70,11 @@ class ImageSampleAnnotation(BaseDBModel, Base):
     __table_args__ = (
         UniqueConstraint('image_sample_id', 'key', 'user_id', name='uq_image_sample_annotation_item'),
     )
+
+
+class AnnotationVote(BaseDBModel, Base):
+    __tablename__ = 'annotation_votes'
+
+    annotation_id = Column(Integer, ForeignKey('image_sample_annotations.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('image_samples.id'), nullable=False)
+    value = Column(SmallInteger, nullable=False, default=0)
